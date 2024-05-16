4b14753cfc71b1e552d7fb815a201ae43264e64f
WernerACT <165774665+WernerACT@users.noreply.github.com>
2024-05-16 14:39:58 +0200

    Update Vite compiler config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import mkcert from "vite-plugin-mkcert/plugin/mkcert/index.js";
 
 export default defineConfig({
+    server: { https: true },
     plugins: [
+        mkcert,
         laravel({
+
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
