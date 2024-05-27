5cbc4765f0f947d58a979f79e11dfdbee1366895
WernerACT <165774665+WernerACT@users.noreply.github.com>
2024-05-27 13:55:51 +0200

    Ticket Index

--- app/Http/Controllers/API/V1/TicketController.php
+++ app/Http/Controllers/API/V1/TicketController.php
@@ -19,6 +19,7 @@ class TicketController extends Controller
     {
         $this->authorize('viewAny', Ticket::class);
 
+
         $query = Ticket::with(['user', 'status', 'department']);
 
         if ($request->filled('start_date')) {
